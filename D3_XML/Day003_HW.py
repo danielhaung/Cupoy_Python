f2fafda8391952e0bab4a7ae2019cb46cd3f44b7
danielhaung <a3084312@yahoo.com.tw>
2020-09-29 16:22:08 +0800

    1
    1

--- D3_XML/Day003_HW.py
+++ D3_XML/Day003_HW.py
@@ -40,17 +40,30 @@ d = dict(xmltodict.parse(xml))
 # 1. 請問高雄市有多少地區有溫度資料？ #
 #####################################
 datasetDescription = d['cwbopendata']['dataset']['locations']['location']
-
+datetime = datasetDescription[0]['weatherElement'][0]['time'][1]['dataTime']
+Temp = datasetDescription[0]['weatherElement'][0]['time'][1]['elementValue']["value"]
 list_all = []
+list_firstTimeTemp= []
 i = 0
-while datasetDescription[i] != None:
+while True:
     try:
+        i+=1
         list_all.append(datasetDescription[i]['locationName'])
+        list_firstTimeTemp.append([datasetDescription[i]['weatherElement'][0]['time'][0]['dataTime'],datasetDescription[i]['weatherElement'][0]['time'][0]['elementValue']["value"]])
     except IndexError:
-        print("超過list")
+        print("搜尋完畢")
+        i-=1
         break
     
-print(list_all)
-    
+# print(list_all,"共"+str(i)+"區")
+print(list_firstTimeTemp)
+
+##############################################
+# 2. 請取出每一個地區所記錄的第一個時間點跟溫度 #
+##############################################
+
+
+
+
 
 

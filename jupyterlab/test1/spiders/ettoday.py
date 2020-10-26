712c5f0cad6ced8c09994b0b5f02a328213aa681
danielhaung <a3084312@yahoo.com.tw>
2020-10-26 13:25:01 +0800

    1
    1

--- jupyterlab/test1/spiders/ettoday.py
+++ jupyterlab/test1/spiders/ettoday.py
@@ -4,7 +4,15 @@ import scrapy
 class EttodaySpider(scrapy.Spider):
     name = 'ettoday'
     allowed_domains = ['www.ettoday.net']
-    start_urls = ['http://www.ettoday.net/']
+    start_urls = ['https://www.ettoday.net/news/20201004/1824032.html']
 
+    
+    #當你對一個url進行請求後，你需要對返回的response進行操作，而parse這個function就是這隻爬蟲默認進行處理response的function，
+    #若你沒有特別指定處理response的function，則由parse這個function進行處理
     def parse(self, response):
-        pass
+        title = response.xpath('//title').get()
+        content = response.xpath('//p').get()
+        print("---------------------------------------------------------------")
+        print(title)
+        print(content)
+        print("---------------------------------------------------------------")

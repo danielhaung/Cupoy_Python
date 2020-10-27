bf0fab316051d354ab0f15b83c3985e894241427
danielhaung <a3084312@yahoo.com.tw>
2020-10-27 13:44:07 +0800

    1
    1

--- jupyterlab/test1/spiders/.ipynb_checkpoints/ettoday-checkpoint.py
+++ jupyterlab/test1/spiders/.ipynb_checkpoints/ettoday-checkpoint.py
@@ -1,4 +1,5 @@
 import scrapy
+from test1.items import Test1Item
 
 
 class EttodaySpider(scrapy.Spider):
@@ -10,9 +11,10 @@ class EttodaySpider(scrapy.Spider):
     #當你對一個url進行請求後，你需要對返回的response進行操作，而parse這個function就是這隻爬蟲默認進行處理response的function，
     #若你沒有特別指定處理response的function，則由parse這個function進行處理
     def parse(self, response):
-        title = response.xpath('//title').get()
-        content = response.xpath('//p').get()
-        print("---------------------------------------------------------------")
-        print(title)
-        print(content)
-        print("---------------------------------------------------------------")
+        item = Test1Item()
+        item['title'] = response.xpath('//title').get()
+        item['content'] = response.xpath('//p').get()
+        print(item['title'])
+        yield item
+
+        
